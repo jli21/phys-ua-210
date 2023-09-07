27c6e1841b36e3a0d01f57f96a00250ae42ce2d7
Zion <jli21bh@gmail.com>
2023-09-07 17:34:01 -0400

    ..

--- ps-1/plot-gaussian.py
+++ ps-1/plot-gaussian.py
@@ -9,5 +9,8 @@ fig = (1 / (std_dev * np.sqrt(2 * np.pi))) * np.exp(-(x - mean)**2 / (2 * std_de
 
 plt.plot(x, fig, label=f'Mean={mean}, Std Dev={std_dev}')
 plt.title('Gaussian Distribution')
+plt.xlabel("X")
+plt.ylabel("Y")
+
 plt.grid(True)
 plt.show()
