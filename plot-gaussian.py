229ad508fd53a3a1180d63123851316ddc534160
Zion <jli21bh@gmail.com>
2023-09-07 17:23:45 -0400

    ..

---
+++ plot-gaussian.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean = 0
+std_dev = 3
+
+x = np.linspace(-10, 10, 1000)  
+fig = (1 / (std_dev * np.sqrt(2 * np.pi))) * np.exp(-(x - mean)**2 / (2 * std_dev**2))
+
+plt.plot(x, fig, label=f'Mean={mean}, Std Dev={std_dev}')
+plt.title('Gaussian Distribution')
+plt.grid(True)
+plt.show()
